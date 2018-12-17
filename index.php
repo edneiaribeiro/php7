7bb894e0cf4b62ace0e8235bf579949e24804482
Edneia Ribeiro <edneia.ribe@gmail.com>
2018-12-17 19:15:22 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -7,10 +7,33 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios); */
 
+/*
+//Carrega um usuario
 $root = new Usuario();
 
 $root->loadById(1);
 
 echo $root;
+*/
 
+/*
+//Carrega uma Lista de usuarios
+
+$lista = Usuario::getList();
+
+echo json_encode($lista, JSON_UNESCAPED_SLASHES);
+*/
+
+/*
+//Carrega uma lista d eusuarios buscando pelo login
+$search = Usuario::search("silva");
+
+echo json_encode($search);
+*/
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("edneiaribeiro", "programadorahermione1");
+
+echo $usuario;
 ?>
